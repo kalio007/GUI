2043406cc3d8ad04ccb21d8545b636dcb36b199e
kalio007 <kalioprince1@gmail.com>
2023-10-22 02:20:04 +0100

    update: added the delete and update operations

--- crud.py
+++ crud.py
@@ -2,12 +2,36 @@ import tkinter as tk
 from tkinter import messagebox
 
 # declaring functions
+items = []
 def add():
-    pass
+    item = entry.get()
+    if item:
+        items.append(item)
+        listbox.insert(tk.END, item)
+        entry.delete(0, tk.END)
+    else:
+        messagebox.showwarning("Please enter and Item")
 def update():
-    pass
+    selected_index = listbox.curselection()
+    if selected_index:
+        new_item = entry.get()
+        if new_item:
+            items[selected_index[0]] = new_item
+            listbox.delete(selected_index)
+            listbox.insert(selected_index[0], new_item)
+            entry.delete(0, tk.END)
+        else:
+            messagebox.showwarning("Warning")
+    else:
+        messagebox.showwarning("warning")
 def delete():
-    pass 
+    selected_index = listbox.curselection()
+    if selected_index:
+        listbox.delete(selected_index)
+        del items[selected_index[0]]
+        entry.delete(0,tk.END)
+    else:
+        messagebox.showwarning("Warning")
 
 root = tk.Tk()
 root.title("CRUD APP")
